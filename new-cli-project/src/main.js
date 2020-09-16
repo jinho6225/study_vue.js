00b78fa694b975af3ca172a55decbcc4b9a06b29
jinho6225 <jinho6225@hotmail.com>
2020-09-16 03:23:02 -0700

    installed vuetify

--- new-cli-project/src/main.js
+++ new-cli-project/src/main.js
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
 
 createApp(App).use(store).use(router).mount('#app')
+
+// I don't use it by default.
+new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount("#app");
